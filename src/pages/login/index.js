2395630b4b6dd86f3a7f86e6fb4e3dd4755e7229
ElenMuradyan <muradyanelen0701@gmail.com>
2024-10-06 18:43:43 +0400

    implement login using useState and useEffect

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,17 +1,36 @@
-import { useState } from "react"
+import { useState, useEffect } from "react";
 
 const Login = () => {
-    let [count, setCount]=useState(0);
+    const [ page, setPage ] = useState(1);
+    const [ showModal, setShowModal ]= useState(false);
 
-    const handleCountchange=()=>{
-setCount(prevstate=>{return prevstate+1});
-setCount(prevstate=>{return prevstate+1});
-setCount(prevstate=>{return prevstate+1});
-setCount(prevstate=>{return prevstate+1});
+    useEffect(() => {
+        fetch(`https://randomuser.me/api/?page=${page}&results=10&seed=abc`)
+        .then(resp=>{
+        return resp.json();
+        })  
+        .then(data=>{
+            console.log(data)
+        })
+    },[page]);
+
+    const handleChangePagination = value => {
+        setPage(value=== 'next' ? page+1 : page-1);
     };
 
     return(<div>
-<button onClick={handleCountchange}>{count}</button>
-    </div>)
-}
-export default Login
+            <button onClick={()=>handleChangePagination('prev')}>prev</button>
+            <span>page={page}</span> 
+            <button onClick={()=>handleChangePagination('next')}>next</button> 
+            <br/>
+            <button onClick={()=>{setShowModal(!showModal)}}>{showModal?'Close Modal':'Show Modal'}</button>
+            {showModal && (
+                <div>
+                    <h2>Modal</h2>
+                </div>
+            )}
+        </div>
+        );
+};
+
+export default Login;
