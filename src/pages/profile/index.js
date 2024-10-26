a07c2d1c52df3d1c69cef63b928c365d793a93c3
ElenMuradyan <muradyanelen0701@gmail.com>
2024-10-26 22:50:35 +0400

    feat: modify profile page to show userProfileInfo using antd Form and react hooks useEffect and useContext

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,11 +1,65 @@
+import { useContext, useEffect } from 'react';
+import { Form, Input, Button } from 'antd';
+import { AuthContext } from '../../context/authContextProvider';
+
 import './index.css';
 
 const Profile = () => {
-return (
-    <div>
-        <h2>Profile</h2>
-    </div>
-)
+    const { userProfileInfo } = useContext(AuthContext);
+    const [ form ] = Form.useForm();
+    
+    useEffect(() => {
+        const { uid, ...restData } = userProfileInfo;
+
+        form.setFieldsValue(restData);
+    }, [userProfileInfo])
+
+    return (
+        <div>
+            <Form layout='vertical' form={form}>
+                <Form.Item
+                label="First Name"
+                name='firstName'
+                >
+                    <Input
+                        placeholder='First Name'
+                    />
+                </Form.Item>
+
+                <Form.Item
+                label="Last Name"
+                name='lastName'
+                >
+                    <Input
+                        placeholder='Last Name'
+                    />
+                </Form.Item>
+
+                <Form.Item
+                label="Email"
+                name='email'
+                >
+                    <Input
+                        readOnly
+                        placeholder='Email'
+                    />
+                </Form.Item>
+
+                <Form.Item
+                label="Phone Number"
+                name='phoneNumber'
+                >
+                    <Input
+                        placeholder='Phone Number'
+                    />
+                </Form.Item>
+
+                <Button type='primary' htmlType='submit'>
+                    Submit
+                </Button>
+            </Form>    
+        </div>
+    )
 };
 
 export default Profile;
