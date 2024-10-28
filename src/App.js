1512e5abd5f4e3d199fa35309ce06731a882a9de
ElenMuradyan <muradyanelen0701@gmail.com>
2024-10-28 22:46:36 +0400

    feat(Profile): modify to update user data in Firebase DB using updateDoc

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { AuthContext } from "./context/authContextProvider";
 import { ROUTE_CONSTANTS, FIRESTORE_PATH_NAMES } from "./core/utilis/constants";
 import { onAuthStateChanged } from 'firebase/auth';
 import { Login, Register } from './pages/auth';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { auth, db } from './services/firebase';
 import { getDoc, doc } from "firebase/firestore";
 import MainLayout from "./layouts/Main";
@@ -18,16 +18,16 @@ const App=()=>{
   const [ loading, setLoading ] = useState(true);
   const [ userProfileInfo, setUserProfileInfo ] = useState({});
   
-  const handleGetUserData = async (uid) => {
+  const handleGetUserData = useCallback(async (uid)=>{
     const docRef = doc(db, FIRESTORE_PATH_NAMES.REGISTERED_USERS, uid);
     const response = await getDoc(docRef);
 
     if(response.exists()){
       setUserProfileInfo(response.data());
-    }
-  }
-  useEffect(()=>{
+    };
+  },[])
 
+  useEffect(()=>{
   onAuthStateChanged(auth, (user) => {
     user?.uid && handleGetUserData(user.uid);
    
@@ -37,7 +37,7 @@ const App=()=>{
   },[]);
 
   return (
-    <AuthContext.Provider value={{ IsAuth, userProfileInfo }}>
+    <AuthContext.Provider value={{ IsAuth, userProfileInfo, handleGetUserData }}>
        <LoadingWrapper loading={loading}>
  <RouterProvider 
     router={
--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,25 +1,55 @@
-import { useContext, useEffect } from 'react';
-import { Form, Input, Button } from 'antd';
+import { useContext, useEffect, useState } from 'react';
+import { Form, Input, Button, notification, Upload } from 'antd';
 import { AuthContext } from '../../context/authContextProvider';
+import { FIRESTORE_PATH_NAMES } from '../../core/utilis/constants';
+import { updateDoc, doc } from 'firebase/firestore';
+import { db } from '../../services/firebase';
 
 import './index.css';
 
 const Profile = () => {
-    const { userProfileInfo } = useContext(AuthContext);
+    const { userProfileInfo, handleGetUserData } = useContext(AuthContext);
     const [ form ] = Form.useForm();
+    const [ buttonLoading, setButtonLoading ] = useState(false);
+    const { uid, ...restData } = userProfileInfo;
+
+    const handleEditUserProfile = async (values) => {
+        setButtonLoading(true);
+        try{
+            const userDocRef = doc(db, FIRESTORE_PATH_NAMES.REGISTERED_USERS, uid);
+            await updateDoc(userDocRef, values);
+            handleGetUserData(uid);
+            notification.success({
+                message:'user data successfully updated'
+            })
+        }catch{
+            notification.error({
+                message:'Error :('
+            })
+        }finally{
+            setButtonLoading(false)
+        }
+    }
     
     useEffect(() => {
-        const { uid, ...restData } = userProfileInfo;
-
         form.setFieldsValue(restData);
-    }, [userProfileInfo])
+    }, [userProfileInfo, form])
 
     return (
         <div>
-            <Form layout='vertical' form={form}>
+            <Form layout='vertical' form={form} onFinish={handleEditUserProfile}>
+                <Form.Item
+                label='Profile Image'
+                >
+                    <Upload></Upload>
+                </Form.Item>
                 <Form.Item
                 label="First Name"
                 name='firstName'
+                rules={[{
+                    required:true,
+                    message:'Please input your First Name'
+                }]}
                 >
                     <Input
                         placeholder='First Name'
@@ -29,6 +59,10 @@ const Profile = () => {
                 <Form.Item
                 label="Last Name"
                 name='lastName'
+                rules={[{
+                    required:true,
+                    message:'Please input your Last Name'
+                }]}
                 >
                     <Input
                         placeholder='Last Name'
@@ -48,13 +82,17 @@ const Profile = () => {
                 <Form.Item
                 label="Phone Number"
                 name='phoneNumber'
+                rules={[{
+                    required:true,
+                    message:'Please input your Phone Number'
+                }]}
                 >
                     <Input
                         placeholder='Phone Number'
                     />
                 </Form.Item>
 
-                <Button type='primary' htmlType='submit'>
+                <Button type='primary' htmlType='submit' loading={buttonLoading}>
                     Submit
                 </Button>
             </Form>    
